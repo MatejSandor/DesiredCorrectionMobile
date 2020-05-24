62fdd29b86006e222c596054832a7d12e49a78ff
Matej Šándor <matejko.sandor@gmail.com>
2020-05-24 15:22:22 +0200

    launch camera with openCV and process frames

---
+++ app/src/main/java/com/sandor/desiredcorrectionmobile/CameraActivity.java
@@ -0,0 +1,99 @@
+package com.sandor.desiredcorrectionmobile;
+
+import android.os.Bundle;
+import android.view.SurfaceView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class CameraActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    JavaCameraView mJavaCameraView;
+    Mat mRGBA, mRGBAT;
+
+    BaseLoaderCallback mBaseLoaderCallback = new BaseLoaderCallback(CameraActivity.this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:
+                    {
+                    mJavaCameraView.enableView();
+                    break;
+                    }
+                default: super.onManagerConnected(status);
+                break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mJavaCameraView = (JavaCameraView) findViewById(R.id.my_camera_view);
+        mJavaCameraView.setVisibility(SurfaceView.VISIBLE);
+        mJavaCameraView.setCvCameraViewListener(CameraActivity.this);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRGBA = new Mat(width,height, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRGBA.release();
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRGBA = inputFrame.rgba();
+        mRGBAT = mRGBA.t();
+        Core.flip(mRGBA,mRGBAT,1);
+        Imgproc.resize(mRGBAT,mRGBAT,mRGBA.size());
+        return null;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(mJavaCameraView != null) {
+            mJavaCameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(OpenCVLoader.initDebug()) {
+            mBaseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        } else {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION,this, mBaseLoaderCallback);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if(mJavaCameraView != null) {
+            mJavaCameraView.disableView();
+        }
+    }
+}
--- app/src/main/java/com/sandor/desiredcorrectionmobile/MainActivity.java
+++ app/src/main/java/com/sandor/desiredcorrectionmobile/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
