c93144c00a859a2b4e0a43933e1fbf0e2601731c
the-chef0 <kucharmichal01@gmail.com>
2020-05-24 19:25:49 +0200

    Add BottomNavigationView

--- app/src/main/java/com/sandor/desiredcorrectionmobile/MainActivity.java
+++ app/src/main/java/com/sandor/desiredcorrectionmobile/MainActivity.java
@@ -1,12 +1,22 @@
 package com.sandor.desiredcorrectionmobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -14,11 +24,40 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private BottomNavigationView mNavBar;
+    private FrameLayout frameLayout;
+    private OverviewFragment overviewFragment;
+    private ExercisesFragment exercisesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.framelayout);
+        mNavBar = findViewById(R.id.nav_bottom);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new OverviewFragment()).commit();
+        }
+
+        mNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_overview:
+                        fragment = new OverviewFragment();
+                        break;
+                    case R.id.nav_exercises:
+                        fragment = new ExercisesFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,fragment).commit();
+                return true;
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +73,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
     }
+
 
 }
